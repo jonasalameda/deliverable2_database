b17125209c56c0266babb3e005b4de014a078d70
jonas <125613982+jonasalameda@users.noreply.github.com>
2024-10-21 21:39:33 -0400

    add Guide table
    added guide table based on the outdated diagram

--- MontrealScenicTours.sql
+++ MontrealScenicTours.sql
@@ -2,3 +2,13 @@ CREATE DATABASE MontrealScenicTours
 USE MontrealScenicTours
 
 -- code :o
+
+CREATE TABLE Guide (
+	Guide_ID CHAR(6) CONSTRAINT Guide_Guide_ID_pk PRIMARY KEY (Guide_ID),
+	Guide_Name VARCHAR(20),
+	Guide_DOH DATE
+)
+
+CREATE TABLE Tour (
+	
+)
