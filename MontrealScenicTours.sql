ac6e4e2a0f4ae18d04ceca2cd0b65f7ab16756f9
jonas <125613982+jonasalameda@users.noreply.github.com>
2024-10-22 20:03:31 -0400

    add Tour table

--- MontrealScenicTours.sql
+++ MontrealScenicTours.sql
@@ -5,10 +5,14 @@ USE MontrealScenicTours
 
 CREATE TABLE Guide (
 	Guide_ID CHAR(6) CONSTRAINT Guide_Guide_ID_pk PRIMARY KEY (Guide_ID),
-	Guide_Name VARCHAR(20),
+	Guide_Name VARCHAR(30),
 	Guide_DOH DATE
 )
 
 CREATE TABLE Tour (
-	
+	Tour_ID CHAR(6) CONSTRAINT Tour_Tour_ID_PK PRIMARY KEY (Tour_ID),
+	Tour_Name VARCHAR(20),
+	Tour_Dur INT,
+	Tour_Fee MONEY,
+	Tour_Order VARCHAR(6)
 )
