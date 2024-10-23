be37c7f86c7a61cc0bb4c0a91cb95bb2a3d03d5b
jonas <125613982+jonasalameda@users.noreply.github.com>
2024-10-22 20:08:42 -0400

    add table Address

--- MontrealScenicTours.sql
+++ MontrealScenicTours.sql
@@ -9,10 +9,20 @@ CREATE TABLE Guide (
 	Guide_DOH DATE
 )
 
+
+
 CREATE TABLE Tour (
 	Tour_ID CHAR(6) CONSTRAINT Tour_Tour_ID_PK PRIMARY KEY (Tour_ID),
 	Tour_Name VARCHAR(20),
 	Tour_Dur INT,
 	Tour_Fee MONEY,
 	Tour_Order VARCHAR(6)
+)
+
+CREATE TABLE Address (
+	Addr_ID CHAR(6) CONSTRAINT Address_Addr_ID_PK PRIMARY KEY (Addr_ID),
+	Addr_Stno INT,
+	Addr_Stname VARCHAR(30),
+	Addr_City VARCHAR(20),
+	Addr_Postal CHAR(6) 
 )
