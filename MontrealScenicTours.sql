551e061d6413e9fa41b4159c5bec3d0839ff0dce
sr-delightfully <%email%>
2024-10-25 14:10:07 -0400

    edited code to better match diagram, as well as fixed some small errors in the syntax.

--- MontrealScenicTours.sql
+++ MontrealScenicTours.sql
@@ -3,24 +3,45 @@ USE MontrealScenicTours
 
 -- code :o
 
-CREATE TABLE Address (
-	Addr_ID CHAR(6) CONSTRAINT Address_Addr_ID_PK PRIMARY KEY (Addr_ID),
-	Addr_Stno INT,
-	Addr_Stname VARCHAR(30),
-	Addr_City VARCHAR(20),
-	Addr_Postal CHAR(6) 
+CREATE TABLE Place (
+	Place_ID CHAR(6) CONSTRAINT Place_Place_ID_PK PRIMARY KEY (Place_ID),
+	Place_Stno INT,
+	Place_Stname VARCHAR(64),
+	Place_City VARCHAR(64),
+	Place_Postal CHAR(7) 
 );
 
 CREATE TABLE Guide (
 	Guide_ID CHAR(6) CONSTRAINT Guide_Guide_ID_PK PRIMARY KEY (Guide_ID),
-	Guide_Name VARCHAR(30),
+	Guide_Name VARCHAR(32),
 	Guide_DOH DATE,
-	Addr_ID CHAR(6) CONSTRAINT Guide_Addr_ID_FK FOREIGN KEY (Addr_ID) REFERENCES (Address)
+	Guide_PHONE VARCHAR(12),
+	Guide_EMAIL VARCHAR(50),
+	Place_ID CHAR(6) CONSTRAINT Guide_Place_ID_FK FOREIGN KEY (Place_ID) REFERENCES Place(Place_ID)
+);
+
+CREATE TABLE Tourist(
+	Tourist_ID CHAR(6) CONSTRAINT Tourist_Tourist_ID_PK PRIMARY KEY (Tourist_ID),
+	Tourist_Name VARCHAR(32),
+	Tourist_Phone VARCHAR(12),
+	Tourist_Email VARCHAR(50),
+	Place_ID CHAR(6) CONSTRAINT Tourist_Place_ID_FK FOREIGN KEY (Place_ID) REFERENCES Place(Place_ID)
+	--The diagram depicts a foreign key for the TRIP_ID, should we add that here?
+);
+
+CREATE TABLE Tour (
+	Tour_ID CHAR(6) CONSTRAINT Tour_Tour_ID_PK PRIMARY KEY (Tour_ID),
+	Tour_Name VARCHAR(32) CONSTRAINT Venue_Ven_Name_UK UNIQUE,
+	Tour_Dur INT,
+	Tour_Fee MONEY,
+	Tour_Order INT IDENTITY (100000, 1),
+	--diagram depicts a "TOUR_QUAL_RQD to check if qualifications are required, i can remove this from the diagram.
+	--again diagram depicts a foreign key for this table (guide_ID & Venue_ID)
 );
 
 CREATE TABLE Qualification (
-	Guide_ID CHAR(6) CONSTRAINT Qualification_Guide_ID_FK FOREIGN KEY (Guide_ID) REFERENCES Guide,
-	Tour_ID CHAR (6) CONSTRAINT Qualification_Tour_ID_FK FOREIGN KEY (Tour_ID) REFERENCES (Tour),
+	Guide_ID CHAR(6) CONSTRAINT Qualification_Guide_ID_FK FOREIGN KEY (Guide_ID) REFERENCES Guide(Guide_ID),
+	Tour_ID CHAR (6) CONSTRAINT Qualification_Tour_ID_FK FOREIGN KEY (Tour_ID) REFERENCES Tour(Tour_ID),
 	Qual_Is_Rqd BIT CHECK (Qual_Is_Rqd = 0 OR Qual_Is_Rqd = 1),
 	Qual_Date DATE,
 	PRIMARY KEY (Guide_ID, Tour_ID)
@@ -28,46 +49,32 @@ CREATE TABLE Qualification (
 
 CREATE TABLE Venue (
 	Ven_ID CHAR (6) CONSTRAINT Venue_Loc_ID_PK PRIMARY KEY (Ven_ID),
-	Ven_Name VARCHAR(30) CONSTRAINT Venue_Ven_Name_UK UNIQUE,
-	Ven_Type VARCHAR(12),
-	Ven_Desc VARCHAR(45),
+	Ven_Name VARCHAR(64) CONSTRAINT Venue_Ven_Name_UK UNIQUE,
+	Ven_Type VARCHAR(32),
+	Ven_Desc VARCHAR(64),
 	Ven_Idx INT IDENTITY (100000, 1),
-	Addr_ID CHAR(6) CONSTRAINT Venue_Addr_ID_FK FOREIGN KEY (Addr_ID) REFERENCES (Address)
+	Addr_ID CHAR(6) CONSTRAINT Venue_Addr_ID_FK FOREIGN KEY (Addr_ID) REFERENCES Place(Place_ID)
 );
 
-CREATE TABLE Tour (
-	Tour_ID CHAR(6) CONSTRAINT Tour_Tour_ID_PK PRIMARY KEY (Tour_ID),
-	Tour_Name VARCHAR(20) CONSTRAINT Venue_Ven_Name_UK UNIQUE,
-	Tour_Dur INT,
-	Tour_Fee MONEY,
-	Tour_Order INT IDENTITY (100000, 1),
-);
-
-CREATE TABLE TourSite (
-	Ven_ID CHAR (6) CONSTRAINT TourSite_Ven_ID_FK FOREIGN KEY (Ven_ID) REFERENCES Venue,
-	Tour_ID CHAR(6) CONSTRAINT TourSite_Tour_ID_FK FOREIGN KEY (Tour_ID) REFERENCES Tour,
+CREATE TABLE TourVenue (
+	Ven_ID CHAR (6) CONSTRAINT TourSite_Ven_ID_FK FOREIGN KEY (Ven_ID) REFERENCES Venue(Ven_ID),
+	Tour_ID CHAR(6) CONSTRAINT TourSite_Tour_ID_FK FOREIGN KEY (Tour_ID) REFERENCES Tour(Tour_ID),
 	PRIMARY KEY (Ven_ID, Tour_ID)
 )
 
 CREATE TABLE Trip (
 	Trip_ID CHAR(6) CONSTRAINT Trip_Trip_ID_PK PRIMARY KEY (Trip_ID),
-	Trip_Name VARCHAR(20) CONSTRAINT Trip_Trip_Name_UK UNIQUE,
 	Trip_Start TIME,
-	Trip_End TIME,
+	Trip_End TIME
+	--Diagram depicts foreign keys for TOUR_ID & GUIDE_ID
 );
 
 CREATE TABLE Visit (
-	Trip_ID CHAR(6) CONSTRAINT Trip_Trip_ID_FK FOREIGN KEY (Trip_ID) REFERENCES (Trip),
-	Tourist_ID CHAR(6) CONSTRAINT Tourist_Tourist_ID_FK FOREIGN KEY (Tourist_ID) REFERENCES (Tourist),
+	Trip_ID CHAR(6) CONSTRAINT Trip_Trip_ID_FK FOREIGN KEY (Trip_ID) REFERENCES Trip(Trip_ID),
+	Tourist_ID CHAR(6) CONSTRAINT Tourist_Tourist_ID_FK FOREIGN KEY (Tourist_ID) REFERENCES Tourist(Tourist_ID),
 	PRIMARY KEY (Trip_ID, Tourist_ID)
 )
 
-CREATE TABLE Tourist(
-	Tourist_ID CHAR(6) CONSTRAINT Tourist_Tourist_ID_PK PRIMARY KEY (Tourist_ID),
-	Tourist_Name VARCHAR(30),
-	Tourist_Phone VARCHAR(11),
-	Addr_ID CHAR(6) CONSTRAINT Tourist_Addr_ID_FK FOREIGN KEY (Addr_ID) REFERENCES (Address)
-);
 
 CREATE SEQUENCE touristID_next --maybe 
 START WITH 1000
@@ -109,7 +116,7 @@ NO CACHE;
 
 INSERT INTO Tour VALUES (CONCAT('TR',NEXT VALUE FOR tourID_next), 'Someone', '1234-5678')
 
-CREATE SEQUENCE addressID_next --maybe 
+CREATE SEQUENCE placeID_next --maybe 
 START WITH 1000
 INCREMENT BY 1
 MINVALUE 1000
@@ -117,7 +124,7 @@ MAXVALUE 9999
 NO CYCLE 
 NO CACHE;
 
-INSERT INTO Address VALUES (CONCAT('AD',NEXT VALUE FOR addressID_next), 'Someone', '1234-5678')
+INSERT INTO Place VALUES (CONCAT('AD',NEXT VALUE FOR placeID_next), 'Someone', '1234-5678')
 
 CREATE SEQUENCE venueID_next --maybe 
 START WITH 1000
