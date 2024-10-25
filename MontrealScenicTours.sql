65a10c8661792519299bbf2dabbb1a17dcda93d4
%username% <%email%>
2024-10-25 15:44:34 -0400

    2 alter tables for unique constraint, ON DELETE added directly to the table and added non null constraints to table directly as well

--- MontrealScenicTours.sql
+++ MontrealScenicTours.sql
@@ -1,8 +1,6 @@
 CREATE DATABASE MontrealScenicTours
 USE MontrealScenicTours
 
--- code :o
-
 CREATE TABLE Place (
 	Place_ID CHAR(6) CONSTRAINT Place_Place_ID_PK PRIMARY KEY (Place_ID),
 	Place_Stno INT,
@@ -13,47 +11,44 @@ CREATE TABLE Place (
 
 CREATE TABLE Guide (
 	Guide_ID CHAR(6) CONSTRAINT Guide_Guide_ID_PK PRIMARY KEY (Guide_ID),
-	Guide_Name VARCHAR(32),
-	Guide_DOH DATE,
-	Guide_PHONE VARCHAR(12),
+	Guide_Name VARCHAR(32) NOT NULL,
+	Guide_DOH DATE NOT NULL,
+	Guide_PHONE VARCHAR(12) NOT NULL,
 	Guide_EMAIL VARCHAR(50),
 	Place_ID CHAR(6) CONSTRAINT Guide_Place_ID_FK FOREIGN KEY (Place_ID) REFERENCES Place(Place_ID)
 );
 
 CREATE TABLE Tourist(
 	Tourist_ID CHAR(6) CONSTRAINT Tourist_Tourist_ID_PK PRIMARY KEY (Tourist_ID),
-	Tourist_Name VARCHAR(32),
-	Tourist_Phone VARCHAR(12),
+	Tourist_Name VARCHAR(32) NOT NULL,
+	Tourist_Phone VARCHAR(12) NOT NULL,
 	Tourist_Email VARCHAR(50),
 	Place_ID CHAR(6) CONSTRAINT Tourist_Place_ID_FK FOREIGN KEY (Place_ID) REFERENCES Place(Place_ID)
-	--The diagram depicts a foreign key for the TRIP_ID, should we add that here?
 );
 
 CREATE TABLE Tour (
 	Tour_ID CHAR(6) CONSTRAINT Tour_Tour_ID_PK PRIMARY KEY (Tour_ID),
-	Tour_Name VARCHAR(32) CONSTRAINT Venue_Ven_Name_UK UNIQUE,
+	Tour_Name VARCHAR(32) CONSTRAINT Venue_Ven_Name_UK NOT NULL,
 	Tour_Dur INT,
 	Tour_Fee MONEY,
 	Tour_Order INT IDENTITY (100000, 1),
-	--diagram depicts a "TOUR_QUAL_RQD to check if qualifications are required, i can remove this from the diagram.
-	--again diagram depicts a foreign key for this table (guide_ID & Venue_ID)
 );
 
 CREATE TABLE Qualification (
-	Guide_ID CHAR(6) CONSTRAINT Qualification_Guide_ID_FK FOREIGN KEY (Guide_ID) REFERENCES Guide(Guide_ID),
+	Guide_ID CHAR(6) CONSTRAINT Qualification_Guide_ID_FK FOREIGN KEY (Guide_ID) REFERENCES Guide(Guide_ID) ON DELETE CASCADE,
 	Tour_ID CHAR (6) CONSTRAINT Qualification_Tour_ID_FK FOREIGN KEY (Tour_ID) REFERENCES Tour(Tour_ID),
 	Qual_Is_Rqd BIT CHECK (Qual_Is_Rqd = 0 OR Qual_Is_Rqd = 1),
-	Qual_Date DATE,
+	Qual_Date DATE NOT NULL,
 	PRIMARY KEY (Guide_ID, Tour_ID)
 );
 
 CREATE TABLE Venue (
 	Ven_ID CHAR (6) CONSTRAINT Venue_Loc_ID_PK PRIMARY KEY (Ven_ID),
-	Ven_Name VARCHAR(64) CONSTRAINT Venue_Ven_Name_UK UNIQUE,
+	Ven_Name VARCHAR(64) CONSTRAINT Venue_Ven_Name_UK NOT NULL,
 	Ven_Type VARCHAR(32),
 	Ven_Desc VARCHAR(64),
 	Ven_Idx INT IDENTITY (100000, 1),
-	Addr_ID CHAR(6) CONSTRAINT Venue_Addr_ID_FK FOREIGN KEY (Addr_ID) REFERENCES Place(Place_ID)
+	Place_ID CHAR(6) CONSTRAINT Venue_Place_ID_FK FOREIGN KEY (Place_ID) REFERENCES Place(Place_ID)
 );
 
 CREATE TABLE TourVenue (
@@ -64,9 +59,8 @@ CREATE TABLE TourVenue (
 
 CREATE TABLE Trip (
 	Trip_ID CHAR(6) CONSTRAINT Trip_Trip_ID_PK PRIMARY KEY (Trip_ID),
-	Trip_Start TIME,
+	Trip_Start TIME NOT NULL,
 	Trip_End TIME
-	--Diagram depicts foreign keys for TOUR_ID & GUIDE_ID
 );
 
 CREATE TABLE Visit (
@@ -75,6 +69,11 @@ CREATE TABLE Visit (
 	PRIMARY KEY (Trip_ID, Tourist_ID)
 )
 
+ALTER TABLE Tour
+ALTER COLUMN Tour_name ADD CONSTRAINT Tour_name_UK UNIQUE;
+
+ALTER TABLE Venue
+ALTER COLUMN Ven_Name ADD CONSTRAINT Ven_name_UK UNIQUE;
 
 CREATE SEQUENCE touristID_next --maybe 
 START WITH 1000
@@ -84,7 +83,6 @@ MAXVALUE 9999
 NO CYCLE 
 NO CACHE;
 
-INSERT INTO Tourist VALUES (CONCAT('TS',NEXT VALUE FOR touristID_next), 'Someone', '1234-5678')
 
 CREATE SEQUENCE guideID_next --maybe 
 START WITH 1000
@@ -94,7 +92,6 @@ MAXVALUE 9999
 NO CYCLE 
 NO CACHE;
 
-INSERT INTO Guide VALUES (CONCAT('GD',NEXT VALUE FOR guideID_next), 'Someone', '1234-5678')
 
 CREATE SEQUENCE tripID_next 
 START WITH 1000
@@ -104,7 +101,6 @@ MAXVALUE 9999
 NO CYCLE 
 NO CACHE;
 
-INSERT INTO Trip VALUES (CONCAT('TP',NEXT VALUE FOR tripID_next), 'Someone', '1234-5678')
 
 CREATE SEQUENCE tourID_next --maybe 
 START WITH 1000
@@ -114,7 +110,14 @@ MAXVALUE 9999
 NO CYCLE 
 NO CACHE;
 
-INSERT INTO Tour VALUES (CONCAT('TR',NEXT VALUE FOR tourID_next), 'Someone', '1234-5678')
+CREATE SEQUENCE qualID_next --maybe 
+START WITH 1000
+INCREMENT BY 1
+MINVALUE 1000
+MAXVALUE 9999
+NO CYCLE 
+NO CACHE;
+
 
 CREATE SEQUENCE placeID_next --maybe 
 START WITH 1000
@@ -124,7 +127,6 @@ MAXVALUE 9999
 NO CYCLE 
 NO CACHE;
 
-INSERT INTO Place VALUES (CONCAT('AD',NEXT VALUE FOR placeID_next), 'Someone', '1234-5678')
 
 CREATE SEQUENCE venueID_next --maybe 
 START WITH 1000
@@ -134,4 +136,38 @@ MAXVALUE 9999
 NO CYCLE 
 NO CACHE;
 
+/*Place*/
+INSERT INTO Place VALUES (CONCAT('AD',NEXT VALUE FOR placeID_next), 'Someone', '1234-5678')
+
+/*Guides*/
+INSERT INTO Guide
+VALUES (CONCAT('GD',NEXT VALUE FOR guideID_next), 'Someone', '1234-5678')
+
+/*Tourist*/
+INSERT INTO Tourist 
+VALUES (CONCAT('TS',NEXT VALUE FOR touristID_next), 'Someone', '1234-5678')
+
+
+/*Tour*/
+INSERT INTO Tour 
+VALUES (CONCAT('TR',NEXT VALUE FOR tourID_next), 'Someone', '1234-5678')
+
+/*Qualification*/
+INSERT INTO Qualification
+VALUES(CONCAT('QU',NEXT VALUE FOR qualID_next), /*--tourID, guideID*/, 1, '01-01-2024')
+
+/*Venue*/
+INSERT INTO Venue
+VALUES(CONCAT('VN',NEXT VALUE FOR veneuID_next), 
+
+/*TourVenue*/
+
 INSERT INTO Venue VALUES (CONCAT('VN',NEXT VALUE FOR venueID_next), 'Someone', '1234-5678')
+
+/*Trip*/
+INSERT INTO Trip 
+VALUES (CONCAT('TP',NEXT VALUE FOR tripID_next), 'Someone', '1234-5678')
+
+/*Visit*/
+
+
